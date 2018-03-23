e1d8aabd125ea819967551845b71295eab784e1d
Edgardo <edgardo231096@hotmail.com>
2018-03-23 15:48:47 -0600

    tarjeta de credito

--- Auditorio-BIMO/js/teclado.js
+++ Auditorio-BIMO/js/teclado.js
@@ -29,7 +29,217 @@ $(document).ready(function(){
 
         var clickHandler = function () {
             var keyVal = this.innerHTML;
-            that.val(that.val() + keyVal); 
+            if (that.val().length<16){
+                that.val(that.val() + keyVal); 
+            }
+        };
+        var closeEvtHandler = function() {
+            kbHide();
+            $("#kb_keypad .kb_key, #kb_keypad .kb_btn").unbind("click")
+        }
+
+        var deleteEvtHandler = function() {
+           that.val( 
+                that.val().substring(0, that.val().length > 0?that.val().length-1:0)
+            );
+        }
+
+
+
+
+        kb.is(":visible") ? ( kbHide(),$("#kb_keypad .kb_key, #kb_keypad .kb_btn").unbind("click") ) : ( kbDisplay(),$("#kb_keypad .kb_key").click(clickHandler),$(".kb_btn_ok").click(closeEvtHandler),$(".kb_btn_delete").click(deleteEvtHandler) );
+
+    }); 
+    
+    $('#card-number').on('click',function(){
+        
+        var kb = $("#kb_keypad");
+        var offset = $(this).offset();
+        var boxHeight = $(this).height();
+        var boxWidth = $(this).width();
+        kb.css("left", offset.left + (boxWidth-kb.width())/2);        
+        //kb.css();    
+        var that = $(this);
+
+
+        var kbDisplay = function() {
+            kb.css("top", $(window).height())
+            kb.css("display", "block");
+            kb.animate({
+                "top": offset.top + boxHeight + 10
+            }, 100);
+        }
+
+        var kbHide = function() {
+            kb.animate({
+                "top": $(window).height()
+            },100, function() {
+                kb.css("display", "none");
+            })
+        }
+
+
+        var clickHandler = function () {
+            var keyVal = this.innerHTML;
+            if (that.val().length<16){
+                that.val(that.val() + keyVal); 
+            }
+        };
+        var closeEvtHandler = function() {
+            kbHide();
+            $("#kb_keypad .kb_key, #kb_keypad .kb_btn").unbind("click")
+        }
+
+        var deleteEvtHandler = function() {
+           that.val( 
+                that.val().substring(0, that.val().length > 0?that.val().length-1:0)
+            );
+        }
+
+
+
+
+        kb.is(":visible") ? ( kbHide(),$("#kb_keypad .kb_key, #kb_keypad .kb_btn").unbind("click") ) : ( kbDisplay(),$("#kb_keypad .kb_key").click(clickHandler),$(".kb_btn_ok").click(closeEvtHandler),$(".kb_btn_delete").click(deleteEvtHandler) );
+
+    }); 
+    
+       $('#card-month').on('click',function(){
+        
+        var kb = $("#kb_keypad");
+        var offset = $(this).offset();
+        var boxHeight = $(this).height();
+        var boxWidth = $(this).width();
+        kb.css("left", offset.left + (boxWidth-kb.width())/2);        
+        //kb.css();    
+        var that = $(this);
+
+
+        var kbDisplay = function() {
+            kb.css("top", $(window).height())
+            kb.css("display", "block");
+            kb.animate({
+                "top": offset.top + boxHeight + 10
+            }, 100);
+        }
+
+        var kbHide = function() {
+            kb.animate({
+                "top": $(window).height()
+            },100, function() {
+                kb.css("display", "none");
+            })
+        }
+
+
+        var clickHandler = function () {
+            var keyVal = this.innerHTML;
+            if (that.val().length<2){
+                that.val(that.val() + keyVal); 
+            }
+        };
+        var closeEvtHandler = function() {
+            kbHide();
+            $("#kb_keypad .kb_key, #kb_keypad .kb_btn").unbind("click")
+        }
+
+        var deleteEvtHandler = function() {
+           that.val( 
+                that.val().substring(0, that.val().length > 0?that.val().length-1:0)
+            );
+        }
+
+
+
+
+        kb.is(":visible") ? ( kbHide(),$("#kb_keypad .kb_key, #kb_keypad .kb_btn").unbind("click") ) : ( kbDisplay(),$("#kb_keypad .kb_key").click(clickHandler),$(".kb_btn_ok").click(closeEvtHandler),$(".kb_btn_delete").click(deleteEvtHandler) );
+
+    }); 
+    
+    $('#card-year').on('click',function(){
+        
+        var kb = $("#kb_keypad");
+        var offset = $(this).offset();
+        var boxHeight = $(this).height();
+        var boxWidth = $(this).width();
+        kb.css("left", offset.left + (boxWidth-kb.width())/2);        
+        //kb.css();    
+        var that = $(this);
+
+
+        var kbDisplay = function() {
+            kb.css("top", $(window).height())
+            kb.css("display", "block");
+            kb.animate({
+                "top": offset.top + boxHeight + 10
+            }, 100);
+        }
+
+        var kbHide = function() {
+            kb.animate({
+                "top": $(window).height()
+            },100, function() {
+                kb.css("display", "none");
+            })
+        }
+
+
+        var clickHandler = function () {
+            var keyVal = this.innerHTML;
+            if (that.val().length<2){
+                that.val(that.val() + keyVal); 
+            }
+        };
+        var closeEvtHandler = function() {
+            kbHide();
+            $("#kb_keypad .kb_key, #kb_keypad .kb_btn").unbind("click")
+        }
+
+        var deleteEvtHandler = function() {
+           that.val( 
+                that.val().substring(0, that.val().length > 0?that.val().length-1:0)
+            );
+        }
+
+
+
+
+        kb.is(":visible") ? ( kbHide(),$("#kb_keypad .kb_key, #kb_keypad .kb_btn").unbind("click") ) : ( kbDisplay(),$("#kb_keypad .kb_key").click(clickHandler),$(".kb_btn_ok").click(closeEvtHandler),$(".kb_btn_delete").click(deleteEvtHandler) );
+
+    }); 
+    $('#card-cvc').on('click',function(){
+        
+        var kb = $("#kb_keypad");
+        var offset = $(this).offset();
+        var boxHeight = $(this).height();
+        var boxWidth = $(this).width();
+        kb.css("left", offset.left + (boxWidth-kb.width())/2);        
+        //kb.css();    
+        var that = $(this);
+
+
+        var kbDisplay = function() {
+            kb.css("top", $(window).height())
+            kb.css("display", "block");
+            kb.animate({
+                "top": offset.top + boxHeight + 10
+            }, 100);
+        }
+
+        var kbHide = function() {
+            kb.animate({
+                "top": $(window).height()
+            },100, function() {
+                kb.css("display", "none");
+            })
+        }
+
+
+        var clickHandler = function () {
+            var keyVal = this.innerHTML;
+            console.log(that.val().length);
+            if (that.val().length<4){
+                that.val(that.val() + keyVal); 
+            }
         };
         var closeEvtHandler = function() {
             kbHide();
@@ -48,4 +258,5 @@ $(document).ready(function(){
         kb.is(":visible") ? ( kbHide(),$("#kb_keypad .kb_key, #kb_keypad .kb_btn").unbind("click") ) : ( kbDisplay(),$("#kb_keypad .kb_key").click(clickHandler),$(".kb_btn_ok").click(closeEvtHandler),$(".kb_btn_delete").click(deleteEvtHandler) );
 
     });
+    
 });
