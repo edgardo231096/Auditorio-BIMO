4ec897ee33098a1e770ab56ded2d67c3304c3e65
Edgardo <edgardo231096@hotmail.com>
2018-03-28 13:16:38 -0600

    bimo.sql e insercion de datos

--- Auditorio-BIMO/sql/bimo.sql
+++ Auditorio-BIMO/sql/bimo.sql
@@ -1,37 +1,18 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 16-03-2018 a las 23:55:07
--- Versión del servidor: 10.1.9-MariaDB
--- Versión de PHP: 7.0.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+﻿
+CREATE DATABASE BIMO; 
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+USE BIMO; 
 
---
--- Base de datos: `dimo`
---
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `asiento`
---
 
 CREATE TABLE `asiento` (
-  `num_asiento` int(11) NOT NULL,
+  `num_asiento` varchar(5) NOT NULL,
   `folio` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `hora` time NOT NULL,
-  `no_tarjeta` varchar(12) NOT NULL
+  `no_tarjeta` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -40,13 +21,17 @@ CREATE TABLE `asiento` (
 -- Estructura de tabla para la tabla `asiento_funcion`
 --
 
+
+
 CREATE TABLE `asiento_funcion` (
-  `num_asiento` int(11) NOT NULL,
+  `num_asiento` varchar(5) NOT NULL,
   `folio` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `hora` time NOT NULL,
   `precio` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 
 -- --------------------------------------------------------
 
@@ -54,12 +39,14 @@ CREATE TABLE `asiento_funcion` (
 -- Estructura de tabla para la tabla `estacionamiento`
 --
 
+
+
 CREATE TABLE `estacionamiento` (
   `num_cajon` int(11) NOT NULL,
   `folio` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `hora` time NOT NULL,
-  `no_tarjeta` varchar(12) NOT NULL
+  `no_tarjeta` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -68,10 +55,14 @@ CREATE TABLE `estacionamiento` (
 -- Estructura de tabla para la tabla `evento`
 --
 
+
+
 CREATE TABLE `evento` (
   `folio` int(11) NOT NULL,
-  `nombre` varchar(200) NOT NULL
+  `nombre` varchar(200) NOT NULL, `artistas` varchar(200) NOT NULL, `descripcion` varchar(200) NOT NULL 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 
 -- --------------------------------------------------------
 
@@ -79,11 +70,15 @@ CREATE TABLE `evento` (
 -- Estructura de tabla para la tabla `funcion`
 --
 
+
+
 CREATE TABLE `funcion` (
   `folio` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `hora` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 
 -- --------------------------------------------------------
 
@@ -91,17 +86,23 @@ CREATE TABLE `funcion` (
 -- Estructura de tabla para la tabla `lista_de_codigos_validos`
 --
 
+
+
 CREATE TABLE `lista_de_codigos_validos` (
-  `codigo` varchar(12) NOT NULL,
+  `codigo` varchar(16) NOT NULL,
   `estado` tinyint(1) DEFAULT '0',
   `num_promo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `promocion`
 --
+
+
 
 CREATE TABLE `promocion` (
   `num_promo` int(11) NOT NULL,
@@ -111,16 +112,22 @@ CREATE TABLE `promocion` (
   `fecha_fin` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `promocion_titular`
 --
 
+
+
 CREATE TABLE `promocion_titular` (
   `num_promo` int(11) NOT NULL,
-  `no_tarjeta` varchar(12) NOT NULL
+  `no_tarjeta` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 
 -- --------------------------------------------------------
 
@@ -128,17 +135,29 @@ CREATE TABLE `promocion_titular` (
 -- Estructura de tabla para la tabla `titular`
 --
 
+
+
 CREATE TABLE `titular` (
-  `no_tarjeta` varchar(12) NOT NULL
+  `no_tarjeta` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Índices para tablas volcadas
---
+
 
 --
--- Indices de la tabla `asiento`
+
+-- Índices para tablas volcadas
+
 --
+
+
+
+-- 
+
+-- Indices de la tabla `asiento`
+
+--
+
+
 ALTER TABLE `asiento`
   ADD PRIMARY KEY (`num_asiento`,`folio`,`fecha`,`hora`,`no_tarjeta`),
   ADD KEY `folio` (`folio`,`fecha`,`hora`),
@@ -292,6 +311,42 @@ ALTER TABLE `promocion_titular`
   ADD CONSTRAINT `promocion_titular_ibfk_1` FOREIGN KEY (`num_promo`) REFERENCES `promocion` (`num_promo`),
   ADD CONSTRAINT `promocion_titular_ibfk_2` FOREIGN KEY (`no_tarjeta`) REFERENCES `titular` (`no_tarjeta`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+INSERT INTO `evento`(`folio`, `nombre`, `artistas`, `descripcion`) VALUES (1,'Lago de los Cisnes',"Ballet Voronezh","Uno de los clásicos más importantes de todos los tiempos: El lago de los cisnes viene a México interpretado por 35 distinguidos bailarines rusos."); 
+
+INSERT INTO `evento`(`folio`, `nombre`, `artistas`, `descripcion`) VALUES (2,'2 Comediante con "Madre"',"Jorge Falcón y Teo González","Jorge Falcón y Teo González llegan para alegrar tu noche con risas garantizadas o te devolvemos tu dinero."); 
+
+INSERT INTO `evento`(`folio`, `nombre`, `artistas`, `descripcion`) VALUES (3,"El Rey León","Más de 100 artistas en escena.","Descubre el musical que conmueve al mundo y siente que te transportas a África con esta conmovedora historia musicalizada."); 
+
+INSERT INTO `evento`(`folio`, `nombre`, `artistas`, `descripcion`) VALUES (4,"Tributo a Paco de Lucía","Michel Camilo & Tomatio",'Michel Camilo (República Dominicana) y Tomatio (España) se unen para realizar un tributo al celebre guitarrista español "Paco de Lucía".');  
+
+INSERT INTO `evento`(`folio`, `nombre`, `artistas`, `descripcion`) VALUES (5,"Molotov Unplugged","Molotov e Invitados",'Revive los temas icónicos de la controversial banda mexicana "Molotov" junto a invitados sorpresa en un ámbiente alternativo.');   
+
+INSERT INTO `evento`(`folio`, `nombre`, `artistas`, `descripcion`) VALUES (6,"Luis Miguel: México Por Siempre","Luis Miguel",'El "Sol de México" llega en su gira nacional para cantar sus nuevos éxtos y sus clásicos que tanto ama el pueblo.');   
+
+INSERT INTO `evento`(`folio`, `nombre`, `artistas`, `descripcion`) VALUES (7,"Ara Malikian","Ara Malikian",'Ara Malikian, presenta su último trabajo "La Increíble Historia de Violín" en su gira mundial. Un maravillo viaje alrededor del mundo.');   
+
+INSERT INTO `funcion`(`folio`, `fecha`, `hora`) VALUES (1,"2018-03-30","20:00");
+
+INSERT INTO `funcion`(`folio`, `fecha`, `hora`) VALUES (1,"2018-03-31","20:00");
+
+INSERT INTO `funcion`(`folio`, `fecha`, `hora`) VALUES (1,"2018-04-01","20:00"); 
+
+INSERT INTO `funcion`(`folio`, `fecha`, `hora`) VALUES (2,"2018-04-15","21:00");
+
+INSERT INTO `funcion`(`folio`, `fecha`, `hora`) VALUES (3,"2018-05-06","20:00"); 
+
+INSERT INTO `funcion`(`folio`, `fecha`, `hora`) VALUES (3,"2018-05-07","20:00"); 
+
+INSERT INTO `funcion`(`folio`, `fecha`, `hora`) VALUES (4,"2018-05-25","21:00");
+
+INSERT INTO `funcion`(`folio`, `fecha`, `hora`) VALUES (4,"2018-05-26","21:00"); 
+
+INSERT INTO `funcion`(`folio`, `fecha`, `hora`) VALUES (5,"2018-06-01","22:00"); 
+
+INSERT INTO `funcion`(`folio`, `fecha`, `hora`) VALUES (6,"2018-06-10","22:00"); 
+
+INSERT INTO `funcion`(`folio`, `fecha`, `hora`) VALUES (7,"2018-07-20","21:00"); 
+
