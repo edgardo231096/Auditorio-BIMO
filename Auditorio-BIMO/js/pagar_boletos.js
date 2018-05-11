809d11d1e34a21935b91c8783ecda262f27e9c28
Edgardo <edgardo231096@hotmail.com>
2018-05-11 13:14:35 -0500

    progress bar en pago de boletos y pop up

--- Auditorio-BIMO/js/pagar_boletos.js
+++ Auditorio-BIMO/js/pagar_boletos.js
@@ -17,7 +17,20 @@ $(document).ready(function(){
                     var asientos = url.searchParams.get("asientos");
                     Eventos.guardarReservacion(cardNumber, cardCvc, function(resp) {
                         console.log("Resultado:", resp);
-                    });
+                    }); 
+                        
+                        // tiempo de actualizacion del progress
+                        TIME = 60;
+
+                        // tag progress y el parrafo
+    
+                        progress = document.getElementById("progress3");
+                        // obtengo el valor maximo
+                        max = progress.max;
+
+                        // aumenta progresivamente el valor del meter
+                        var loading = false;
+                        $("#continue").on("click", functionProgress3(0,TIME, max, loading, cardNumber, funcion_id ));
                 }
             }), a(".button-sent #back").click(function(b) {
                 a("#area .master-card").css("transform", "rotateY(0deg)"), a(this).hide()
@@ -25,4 +38,68 @@ $(document).ready(function(){
             })
         })
     }(jQuery);
+}); 
+
+   
+    function functionProgress3(i, TIME, max, loading, cardNumber, funcion_id) {
+        $('#continue').prop('disabled', true);
+        $('#back').prop('disabled', true);
+        $('#helpbutton').prop("onclick", null);
+        $('#homeBtn').removeAttr("href");
+        if(!loading){
+          // actualizo
+          function decrementar(i) {
+            console.log("Counting", i);
+            progress.value = i;
+            if (i >= max){
+                console.log("i->",i," max->",max);
+                $('#progress3').hide();  
+                loading = false;
+                popupOpenClose($(".popup"), cardNumber, funcion_id);
+            }
+              if(i < max)
+              setTimeout(function() { decrementar(i + 1) }, TIME);
+          }
+
+         $('#progress3').show();  
+          loading = true;
+          decrementar(0);
+        }
+          // decremento el valor de i al minimo  
+    }  
+
+function popupOpenClose(popup, cardNumber, funcion_id) {
+	
+	/* Add div inside popup for layout if one doesn't exist */
+	if ($(".wrapper").length == 0){
+		$(popup).wrapInner("<div class='wrapper'></div>");
+	}
+	
+	/* Open popup */
+	$(popup).show();
+    
+    /*go to parking view*/
+    $(popup).find("button[name=go]").on("click", function() {
+		if ($(".formElementError").is(':visible')) {
+			$(".formElementError").remove();
+		}
+		$(popup).hide();
+        console.log("cardNumber -> ", cardNumber, "funcion_id -> ", funcion_id);
+        window.location.href = "parking.html";
+	});
+	
+	/* Close popup and remove errors if user clicks on cancel or close buttons */
+	$(popup).find("button[name=close]").on("click", function() {
+		if ($(".formElementError").is(':visible')) {
+			$(".formElementError").remove();
+		}
+		$(popup).hide();
+        window.location.href = "../index.html";
+	});
+}
+
+$(document).ready(function () {
+	$("[data-js=open]").on("click", function() {
+		popupOpenClose($(".popup"));
+	});
 });
